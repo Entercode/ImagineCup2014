54bde8efe3dd5cb48cc78780805cef6087f7a0a2
Quotenter <quotenter@gmail.com>
2013-12-23 18:11:53 +0900

    Initial Commit

---
+++ ios/SYNAPSE/SYNAPSE/DataViewController.h
@@ -0,0 +1,16 @@
+//
+//  DataViewController.h
+//  SYNAPSE
+//
+//  Created by 青井 佑 on 2013/12/23.
+//  Copyright (c) 2013年 Quotenter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) id dataObject;
+
+@end
