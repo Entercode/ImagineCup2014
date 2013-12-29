b7560e868c1dad19a06a2b878b499aa8a3d3e829
T-Okada55 <takahirookada19940421@gmail.com>
2013-12-30 03:50:43 +0900

    Create SQL Database

---
+++ azure/SynapseServer/SQLQuery/dbo.StreetPass.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[StreetPass]
+(
+	[UserBindId] INT NOT NULL, 
+    [PassedDeviceId] BIGINT NOT NULL, 
+    [PassedTime] DATETIME2(2) NOT NULL, 
+    CONSTRAINT [FK_StreetPass_ToAccountDevice] FOREIGN KEY ([UserBindId]) REFERENCES [AccountDevice]([BindId]), 
+    CONSTRAINT [PK_StreetPass] PRIMARY KEY ([UserBindId],[PassedTime]) 
+)
