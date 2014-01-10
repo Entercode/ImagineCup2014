805ca907f7d189bd7c913ac7034308b35e57aa19
Quotenter <quotenter@gmail.com>
2014-01-10 13:55:01 +0900

    hoge2

---
+++ ios/SYNAPSE/SYNAPSE/hoge2.m
@@ -0,0 +1,13 @@
+//
+//  hoge2.m
+//  SYNAPSE
+//
+//  Created by 青井 佑 on 2014/01/10.
+//  Copyright (c) 2014年 Quotenter. All rights reserved.
+//
+
+#import "hoge2.h"
+
+@implementation hoge2
+
+@end
