1f7fe7eed02c6810b17689482ff85ad2b9d90d15
T-Okada55 <takahirookada19940421@gmail.com>
2014-01-16 22:42:00 +0900

    プロフィールとツイートのテーブルを追加

---
+++ azure/SynapseServer/SQLQuery/dbo.AccoutProfile.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[AccountProfile]
+(
+	[UserId] VARCHAR(50) NOT NULL PRIMARY KEY, 
+    [Profile] NVARCHAR(MAX) NULL, 
+    [ImagePath] NVARCHAR(50) NULL, 
+    CONSTRAINT [FK_AccountProfile_ToAccount] FOREIGN KEY ([UserId]) REFERENCES [Account]([UserId])
+)
---
+++ azure/SynapseServer/SQLQuery/dbo.Tweet.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Tweet]
+(
+	[BindId] INT NOT NULL PRIMARY KEY, 
+    [ClientTweetTime] DATETIME2(2) NOT NULL, 
+    [ServerRecieveTime] DATETIME2(2) NOT NULL, 
+    [Tweet] NVARCHAR(MAX) NOT NULL, 
+    CONSTRAINT [FK_Tweet_ToAccountDevice] FOREIGN KEY ([BindId]) REFERENCES [AccountDevice]([BindId])
+)
