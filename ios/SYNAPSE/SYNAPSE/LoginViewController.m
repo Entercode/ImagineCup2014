2ff5f32b713fb47ae11c257ba202d4de59123c64
Quotenter <quotenter@gmail.com>
2014-01-22 12:32:09 +0900

    ログイン処理編集

--- ios/SYNAPSE/SYNAPSE/LoginViewController.m
+++ ios/SYNAPSE/SYNAPSE/LoginViewController.m
@@ -39,28 +39,48 @@
 - (IBAction)pushedLoginButton:(id)sender {
     
     // Generate SHA-1 hash from UUID
-    NSUUID *uuid = [[UIDevice currentDevice] identifierForVendor];
-    NSString *did_h = [SHA1Generator sha1WithString:[uuid UUIDString]];
-    
+    //NSUUID *uuid = [[UIDevice currentDevice] identifierForVendor];
+    //NSString *did_h = [SHA1Generator sha1WithString:[uuid UUIDString]];
+    NSString *did_h = @"2222222222222222222222222222222222222222";
     
     // Send login data with POST
     NSURL *url = [NSURL URLWithString:@"http://synapse-server.cloudapp.net/Set/Login.aspx"];
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
     NSString *keyValue = [NSString stringWithFormat:@"uid_h=%@&did_h=%@&pass_h=%@", [SHA1Generator sha1WithString:self.userIDField.text], did_h, [SHA1Generator sha1WithString:self.passwordField.text]];
+    
     NSData *post = [keyValue dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
     
     [request setHTTPMethod:@"POST"];
     [request setHTTPBody:post];
     
     // Post data
-    [NSURLConnection sendAsynchronousRequest:request  queue:[[NSOperationQueue alloc] init]  completionHandler:^(NSURLResponse *response, NSData *data, NSError *error){
+    [NSURLConnection sendAsynchronousRequest:request  queue:[[NSOperationQueue alloc] init] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error){
     
         // Get Cookie
         
         NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
         NSLog(@"%@", httpResponse);
-        NSArray *cookies = [NSHTTPCookie cookiesWithResponseHeaderFields:httpResponse.allHeaderFields forURL:response.URL];
-        NSLog(@"%@", cookies);
+        //NSArray *cookies = [NSHTTPCookie ];
+        NSLog(@"%@", data);
+        //NSArray *cookies = [NSHTTPCookie ]
+        //NSLog(@"cookies: %@", cookies);
+        NSHTTPCookieStorage *storage = [ NSHTTPCookieStorage sharedHTTPCookieStorage ];
+        NSArray *cookies = [ storage cookiesForURL:[ NSURL URLWithString : @"http://synapse-server.cloudapp.net/" ] ];
+        
+        // Cookie処理ループ
+        for (NSHTTPCookie *cookie in cookies) {
+            // 目的のCookieが見つかったら値を返す
+            if ([ cookie.name isEqualToString: @"sid" ]) {
+                NSString     *fugafuga = cookie.value;
+                NSLog(@"sid=%@", fugafuga);
+                //return fugafuga;
+            }
+        }
+        /*for (NSHTTPCookie *cookie in cookies) {
+            if([cookie.name isEqualToString:@"Content-Type"]) {
+                NSLog(@"sid: %@", cookie.value);
+            }
+        }*/
         /*for (int i = 0; i < cookies.count; i++) {
             NSHTTPCookie *cookie = [cookies objectAtIndex:i];
             NSLog(@"cookie: name=%@, value=%@", cookie.name, cookie.value);
