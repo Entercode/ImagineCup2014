8cc17a83084c4a5a64ea4eb6773d4715d0717049
YoshikiAndo <y6619a@gmail.com>
2014-01-29 00:15:13 +0900

    近接デバイスの出現・消滅を検出するように修正 ListBoxの廃止、MessageBlockに非同期で表示する関数の追加

--- storeapp/App1/App1/MainPage.xaml.cs
+++ storeapp/App1/App1/MainPage.xaml.cs
@@ -14,8 +14,6 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 using Windows.UI.Popups;
-//using Windows.Devices.Enumeration;
-//using Windows.Devices.WiFiDirect;
 using Windows.Networking.Proximity;
 
 // 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
@@ -27,17 +25,32 @@ namespace App1
     /// </summary>
     public sealed partial class MainPage : Page
     {
-
+        Windows.Networking.Proximity.ProximityDevice proximityDevice;
         public MainPage()
         {
             this.InitializeComponent();
             //自機のDisplayNameを表示
-            textblock.Text+=PeerFinder.DisplayName;
-
+            textblock.Text += PeerFinder.DisplayName;
+            
         }
         
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
+            proximityDevice = Windows.Networking.Proximity.ProximityDevice.GetDefault();
+            if (proximityDevice != null)
+            {
+                proximityDevice.DeviceArrived += ProximityDeviceArrived;
+                proximityDevice.DeviceDeparted += ProximityDeviceDeparted;
+
+                WriteMessageText("Proximity device initialized.\n");
+            }
+            else
+            {
+                WriteMessageText("Failed to initialized proximity device.\n");
+            }
+           
+
+
             //アプリが動作しているPCでWi-Fi Directが使えるかをチェック
             if ((PeerFinder.SupportedDiscoveryTypes & PeerDiscoveryTypes.Browse) == PeerDiscoveryTypes.Browse) { }
             else {
@@ -45,10 +58,7 @@ namespace App1
                 await dialog.ShowAsync();
             }
 
-            //if (PeerFinder.AllowWiFiDirect) { }
-
-
-            PeerFinder.ConnectionRequested +=new TypedEventHandler<object, ConnectionRequestedEventArgs>(PeerFinder_ConnectionRequested);
+            //PeerFinder.ConnectionRequested +=new TypedEventHandler<object, ConnectionRequestedEventArgs>(PeerFinder_ConnectionRequested);
             //他端末から自機端末がブラウズできるようにする
             PeerFinder.Start();
             
@@ -56,29 +66,54 @@ namespace App1
             //peersはPeerFinderInformationの員スタンす
             var peers = await PeerFinder.FindAllPeersAsync();
 
-            if (peers.Count > 0)
+            if (peers.Any())
             {
-                for (int i = 0; i < peers.Count; i++)
+                foreach (var i in peers)
                 {
-                    //ListBoxにDisplayNameを表示
-                    FoundDeviceList.Items.Add(peers[i].DisplayName);
+                    //FoundDeviceList.Items.Add(i.Id);
+                    WriteMessageText("Found Device Id is "+i.Id);
                 }
 
             }
             else
             {
-                FoundDeviceList.Items.Add("Device Not Found");
-                //var dialog = new MessageDialog("Device Not Found");
-                //await dialog.ShowAsync();
+                //FoundDeviceList.Items.Add("Device Not Found");
+                WriteMessageText("Device Not Found.\n");
             }
 
         }
-        private void PeerFinder_ConnectionRequested(object sender,Windows.Networking.Proximity.ConnectionRequestedEventArgs e)
-            {
+        /*private void PeerFinder_ConnectionRequested(object sender,Windows.Networking.Proximity.ConnectionRequestedEventArgs e)
+        {
               Windows.Networking.Proximity.PeerInformation requestingPeer;
               requestingPeer = e.PeerInformation;
-            //WriteMessageText("Connection requested by " + requestingPeer.DisplayName + ". " + "Click 'Accept Connection' to connect.");
-            }
+              WriteMessageText("Connection requested by " + requestingPeer.DisplayName + ". " + "Click 'Accept Connection' to connect.");
+        }*/
 
+        //近接デバイスの出現を検出し、メッセージ
+        private void ProximityDeviceArrived(Windows.Networking.Proximity.ProximityDevice device)
+        {
+            WriteMessageText("Proximate device arrived. id = " + device.DeviceId + "\n");
+        }
+        //近接デバイスの消滅をけんすつし、メッセージ
+        private void ProximityDeviceDeparted(Windows.Networking.Proximity.ProximityDevice device)
+        {
+            WriteMessageText("Proximate device departed. id = " + device.DeviceId + "\n");
+        }
+        
+        // Write a message to MessageBlock on the UI thread.
+        private Windows.UI.Core.CoreDispatcher messageDispatcher = Window.Current.CoreWindow.Dispatcher;
+
+        //MessageBlockにメッセージを書き込む関数
+        async private void WriteMessageText(string message, bool overwrite = false)
+        {
+            await messageDispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal,
+                () =>
+                {
+                    if (overwrite)
+                        MessageBlock.Text = message;
+                    else
+                        MessageBlock.Text += message;
+                });
+        }
     }
 }
